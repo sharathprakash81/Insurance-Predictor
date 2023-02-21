20c0890e2d4ce6028459d0c9d4a300e9724232c7
sharathprakash81 <sharathprakash81@gmail.com>
2023-02-21 17:18:48 +0000

    Data Ingestion

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os, sys
 from Insurance.utils import get_collection_as_dataframe
 from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
+from Insurance.components.data_ingestion import DataIngestion
 
 # def test_logger_and_exception():
 #     try:
@@ -23,6 +24,9 @@ if __name__ == "__main__":
         training_pipeline_config = config_entity.TrainingPipelineConfig()
         data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         print(data_ingestion_config.to_dict())
+        
+        data_ingestion = DataIngestion(data_ingestion_config = data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
     
     except Exception as e:
         print(e)
