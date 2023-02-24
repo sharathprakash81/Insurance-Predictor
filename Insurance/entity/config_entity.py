c07ee88fc795d58f83f79fc7848ce62aa61835a8
sharathprakash81 <sharathprakash81@gmail.com>
2023-02-24 15:14:48 +0000

    testing inprogress

--- Insurance/entity/config_entity.py
+++ Insurance/entity/config_entity.py
@@ -47,7 +47,7 @@ class DataIngestionConfig:
 class DatavalidationConfig:
     
     def __init__(self,training_pipeline_config: TrainingPipelineConfig):
-        self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir, "datavalidation")
+        self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir, "data_validation")
         self.report_file_path = os.path.join(self.data_validation_dir, "report.yml")
         self.missing_threshold:float = 0.2
         self.base_file_path = os.path.join("insurance.csv")
